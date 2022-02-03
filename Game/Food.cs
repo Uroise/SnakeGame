90f88f69cad0a61a975145a14653e5378e296fce
peyDEV <93034361+peydevgit@users.noreply.github.com>
2022-02-03 21:46:36 +0100

    Created Food
    Created food class with basic constructor and empty update function.

---
+++ Game/Food.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeGame
+{
+    internal class Food : GameObject
+    {
+        public Food()
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+    }
+}
