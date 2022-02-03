b21146c320696bc3415d78a12281f1cdf6baf6ac
peyDEV <93034361+peydevgit@users.noreply.github.com>
2022-02-03 22:17:33 +0100

    Update Food.cs
    Inserted positions for food constructor.

--- Game/Food.cs
+++ Game/Food.cs
@@ -6,10 +6,11 @@ namespace SnakeGame
 {
     internal class Food : GameObject
     {
-        // Food Konstruktor
-        public Food()
+        // Food Konstruktor med x,y som parameterar som håller reda på vilken position food är på.
+        public Food(int x,int y)
         {
-
+            base.Position.positionX = x;
+            base.Position.positionY = y;   
         }
         // Update metoden för Food.
         public void Update()
