dfa7f699ed6c8e2eb870243157ece189fb883ef3
MrAymenDer <91115646+MrAymenDer@users.noreply.github.com>
2022-02-04 23:47:36 +0100

    Add score and time
    Add score and time

--- Game/ConsoleRenderer.cs
+++ Game/ConsoleRenderer.cs
@@ -13,8 +13,20 @@ namespace SnakeGame
         {
             // TODO Konfigurera Console-fönstret enligt världens storlek
             world = gameWorld;
+            //Console.Write("test");
             Console.Clear();
 
+        }
+        public void Time(string time)
+        {
+            Console.SetCursorPosition(35, (world.height));
+            Console.Write("Time : {0}", time);
+        }
+
+        public void Score( int score)
+        {
+            Console.SetCursorPosition(5,(world.height));
+            Console.Write("Score : {0}",score);
         }
 
         public void Render()
@@ -22,24 +34,24 @@ namespace SnakeGame
             Console.CursorVisible = false;
             // TODO Rendera spelvärlden (och poängräkningen)
             Console.Clear();
-            for (int i = 1; i <= (world.width + 2); i++)
+            for (int i = 1; i <= (world.width - 1); i++)
             {
                 Console.SetCursorPosition(i, 1);
-                Console.Write("-");
+                Console.Write("_");
             }
-            for (int i = 1; i <= (world.width + 2); i++)
+            for (int i = 1; i <= (world.width - 1); i++)
             {
-                Console.SetCursorPosition(i, (world.height + 2));
-                Console.Write("-");
+                Console.SetCursorPosition(i, (world.height - 1 ));
+                Console.Write("_");
             }
-            for (int i = 1; i <= (world.height + 2); i++)
+            for (int i = 2; i <= (world.height - 1); i++)
             {
                 Console.SetCursorPosition(1, i);
                 Console.Write("|");
             }
-            for (int i = 1; i <= (world.height + 2); i++)
+            for (int i = 2; i <= (world.height - 1); i++)
             {
-                Console.SetCursorPosition((world.width + 2), i);
+                Console.SetCursorPosition((world.width - 1), i);
                 Console.Write("|");
             }
             // Använd Console.SetCursorPosition(int x, int y) and Console.Write(char)
